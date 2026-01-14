5c5e3cb7f8b7fe779702e85df56bf30ad4fac0c4
Lucas Adrian Sebastian Casas <lucas.a.s.casas@outlook.es>
2026-01-14 20:38:59 -0300

    feat: implemented class BaseRobot with 2D movement logic

--- app/main.py
+++ app/main.py
@@ -2,4 +2,78 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+
+class BaseRobot:
+    """
+    Representation of a common robot that can move along two cartesian axes.
+
+    Attributes:
+        name (str): name of the robot.
+        weight (int | float): represents how many kg it weighs.
+        coords (list): coordinates with two cartesian axes [x, y].
+    """
+
+    def __init__(
+        self,
+        name: str,
+        weight: int | float,
+        coords: list
+    ) -> None:
+        self.name = name
+        self.weight = weight
+        if self.coords is None:
+            self.coords = [0, 0]
+        self.coords = coords
+
+    def go_forward(
+        self,
+        step: int = 1
+    ) -> None:
+        """
+        Moves the robot forward by increasing the Y axis.
+
+        Args:
+            step (int): Number of steps to move. Defaults to 1.
+        """
+        self.coords[1] += step
+
+    def go_back(
+        self,
+        step: int = 1
+    ) -> None:
+        """
+        Moves the robot backward by decreasing the Y axis.
+
+        Args:
+            step (int): Number of steps to move. Defaults to 1.
+        """
+        self.coords[1] -= step
+
+    def go_right(
+        self,
+        step: int = 1
+    ) -> None:
+        """
+        Moves the robot to the right by increasing the X axis.
+
+        Args:
+            step (int): Number of steps to move. Defaults to 1.
+        """
+        self.coors[0] += step
+
+    def go_left(
+        self,
+        step: int = 1
+    ) -> None:
+        """
+        Moves the robot to the left by decreasing the X axis.
+
+        Args:
+            step (int): Number of steps to move. Defaults to 1.
+        """
+        self.coors[0] -= step
+
+    def get_info(
+        self
+    ) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
