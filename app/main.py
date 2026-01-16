17424fb67e05931c9b0917510937ff34db559f41
Lucas Adrian Sebastian Casas <lucas.a.s.casas@outlook.es>
2026-01-16 14:59:08 -0300

    feat: implement BaseRobot subclass, FlyingRobot class with 3d movement.

--- app/main.py
+++ app/main.py
@@ -59,7 +59,7 @@ class BaseRobot:
         Args:
             step (int): Number of steps to move. Defaults to 1.
         """
-        self.coors[0] += step
+        self.coords[0] += step
 
     def go_left(
         self,
@@ -71,9 +71,56 @@ class BaseRobot:
         Args:
             step (int): Number of steps to move. Defaults to 1.
         """
-        self.coors[0] -= step
+        self.coords[0] -= step
 
     def get_info(
         self
     ) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
+
+
+class FlyingRobot(BaseRobot):
+    """
+    It represents a robot with the ability to fly.
+
+    Aargs:
+        name (str): the robot's name.
+        weight (int | float): the robot's weight.
+        coords (list[int]): the cardinal position along the
+            three axes [x, y, z].
+    """
+
+    def __init__(
+        self,
+        name: str,
+        weight: int | float,
+        coords: list[int]
+    ) -> None:
+        super().__init__(name, weight)
+        if coords is None:
+            self.coords = [0, 0, 0]
+        self.coords = coords
+
+    def go_up(
+        self,
+        step: int = 1
+    ) -> None:
+        """
+        Move the robot upwards by incrementing the z-axis.
+
+        Args:
+            step (int): Number of steps to move. The default value is 1.
+        """
+        self.coords[2] += step
+
+    def go_down(
+        self,
+        step: int = 1
+    ) -> None:
+        """
+        Move the robot downwards by decreasing the z-axis.
+
+        Args:
+        step (int): Number of steps to move. The default value is 1.
+        """
+        self.coords[2] -= step
